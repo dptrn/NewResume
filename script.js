79db0ffd004e1829728fbddeec615098ffc8494b
dptrn <dptrn1998@gmail.com>
2024-05-03 14:00:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,399 @@
+const h1=document.createElement("h1")
+h1.innerHTML= "IMAGE NOT INCLUDED"
+document.querySelector("body").appendChild(h1);
+
+const main=document.createElement("div")
+main.setAttribute("class","main")
+document.querySelector("body").appendChild(main);
+
+const mainclass=document.createElement("div")
+mainclass.setAttribute("class","mainclass")
+document.querySelector(".main").appendChild(mainclass);
+
+const mainclass1=document.createElement("div")
+mainclass1.setAttribute("class","mainclass1")
+document.querySelector(".mainclass").appendChild(mainclass1);
+
+const img=document.createElement("img")
+img.setAttribute("src","https://img.freepik.com/premium-photo/woman-yellow-suit-stands-front-yellow-background_662214-635854.jpg?w=360");
+img.setAttribute("id","img")
+document.querySelector(".mainclass1").appendChild(img)
+
+const main1=document.createElement("div")
+main1.setAttribute("class","main1")
+document.querySelector(".mainclass1").appendChild(main1);
+
+const h5=document.createElement("h5")
+h5.innerHTML="EDUCATION_"
+document.querySelector(".main1").appendChild(h5);
+
+const br1=document.createElement("br")
+document.querySelector(".main1").appendChild(br1);
+
+const para3=document.createElement("div")
+para3.setAttribute("class","para3")
+document.querySelector(".main1").appendChild(para3);
+
+const p1=document.createElement("p")
+p1.innerHTML="Creative Graphic Arts"
+document.querySelector(".para3").appendChild(p1);
+
+const p2=document.createElement("p")
+p2.innerHTML="University Name Here"
+document.querySelector(".main1").appendChild(p2);
+
+const p3=document.createElement("p")
+p3.innerHTML="2019-2020"
+document.querySelector(".main1").appendChild(p3);
+
+const br2=document.createElement("br")
+document.querySelector(".main1").appendChild(br2);
+
+const para4=document.createElement("div")
+para4.setAttribute("class","para4")
+document.querySelector(".main1").appendChild(para4);
+
+const p4=document.createElement("p")
+p4.innerHTML="Bachelro of Science"
+document.querySelector(".para4").appendChild(p4);
+
+const p5=document.createElement("p")
+p5.innerHTML="University Name Here"
+document.querySelector(".main1").appendChild(p5);
+
+const p6=document.createElement("p")
+p6.innerHTML="2015-2019"
+document.querySelector(".main1").appendChild(p6);
+
+const br5=document.createElement("br")
+document.querySelector(".main1").appendChild(br5);
+
+const h6=document.createElement("h5")
+h6.innerHTML="REFERENCE_"
+document.querySelector(".main1").appendChild(h6);
+
+const br6=document.createElement("br")
+document.querySelector(".main1").appendChild(br6);
+
+const refer=document.createElement("div")
+refer.setAttribute("class","refer")
+document.querySelector(".main1").appendChild(refer);
+
+const refer1=document.createElement("div")
+refer1.setAttribute("class","refer1")
+document.querySelector(".refer").appendChild(refer1);
+
+const para5=document.createElement("div")
+para5.setAttribute("class","para5")
+document.querySelector(".refer").appendChild(para5);
+
+const content1=document.createElement("div")
+content1.setAttribute("class","content1")
+document.querySelector(".para5").appendChild(content1);
+
+const p7=document.createElement("p")
+p7.innerHTML="M David Parker"
+document.querySelector(".content1").appendChild(p7)
+
+const p8=document.createElement("p")
+p8.innerHTML="Senior Web Designer"
+document.querySelector(".para5").appendChild(p8)
+
+const p9=document.createElement("p")
+p9.innerHTML="Company Name"
+document.querySelector(".para5").appendChild(p9)
+
+const br7=document.createElement("br")
+document.querySelector(".main1").appendChild(br7);
+
+const reference=document.createElement("div")
+reference.setAttribute("class","reference")
+document.querySelector(".main1").appendChild(reference);
+
+const refer2=document.createElement("div")
+refer2.setAttribute("class","refer2")
+document.querySelector(".reference").appendChild(refer2);
+
+const para6=document.createElement("div")
+para6.setAttribute("class","para6")
+document.querySelector(".reference").appendChild(para6);
+
+const content2=document.createElement("div")
+content2.setAttribute("class","content2")
+document.querySelector(".para6").appendChild(content2);
+
+const p10=document.createElement("p")
+p10.innerHTML="D Jhonny Parker"
+document.querySelector(".content2").appendChild(p10);
+
+const p11=document.createElement("p")
+p11.innerHTML="Senior Web Designer"
+document.querySelector(".para6").appendChild(p11);
+
+const p12=document.createElement("p")
+p12.innerHTML="Company Name"
+document.querySelector(".para6").appendChild(p12);
+
+const br8=document.createElement("br")
+document.querySelector(".main1").appendChild(br8);
+
+const h7=document.createElement("h5")
+h7.innerHTML="CONTACT_"
+document.querySelector(".main1").appendChild(h7);
+
+const br9=document.createElement("br")
+document.querySelector(".main1").appendChild(br9);
+
+const p13=document.createElement("hp")
+p13.innerHTML="Street Address 0, New York, United States"
+document.querySelector(".main1").appendChild(p13);
+
+const br10=document.createElement("br")
+document.querySelector(".main1").appendChild(br10);
+
+const p14=document.createElement("hp")
+p14.innerHTML="+0123 413 8436"
+document.querySelector(".main1").appendChild(p14);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const mainclass2=document.createElement("div")
+mainclass2.setAttribute("class","mainclass2")
+document.querySelector(".mainclass").appendChild(mainclass2);
+
+const h3=document.createElement("h2")
+h3.innerHTML="ANNE"
+document.querySelector(".mainclass2").appendChild(h3);
+
+// const br25=document.createElement("br")
+// document.querySelector(".mainclass2").appendChild(br25);
+
+const h20=document.createElement("h2")
+h20.innerHTML="ROBERTSON"
+document.querySelector(".mainclass2").appendChild(h20);
+
+const br26=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br26);
+
+const h4=document.createElement("h4")
+h4.innerHTML="WEB DESIGNER"
+document.querySelector(".mainclass2").appendChild(h4);
+
+const br11=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br11);
+
+const br12=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br12);
+
+const br13=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br13);
+
+const br14=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br14);
+
+const br15=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br15);
+
+// const br16=document.createElement("br")
+// document.querySelector(".mainclass2").appendChild(br16);
+
+// const br17=document.createElement("br")
+// document.querySelector(".mainclass2").appendChild(br17);
+
+const horiline=document.createElement("div")
+horiline.setAttribute("class","horiline")
+document.querySelector(".mainclass2").appendChild(horiline);
+
+const br18=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br18);
+
+const h8=document.createElement("h5")
+h8.innerHTML="JOB EXPERIENCE_"
+document.querySelector(".mainclass2").appendChild(h8);
+
+const br19=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br19);
+
+const p15=document.createElement("p")
+p15.innerHTML="2019-Present"
+document.querySelector(".mainclass2").appendChild(p15);
+
+const p16=document.createElement("p")
+p16.innerHTML="Enter Your Job Position Here"
+document.querySelector(".mainclass2").appendChild(p16);
+
+const p17=document.createElement("p")
+p17.innerHTML="Company Name"
+document.querySelector(".mainclass2").appendChild(p17);
+
+const br20=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br20);
+
+
+const lorem =document.createElement("div")
+lorem.setAttribute("class","lorem")
+document.querySelector(".mainclass2").appendChild(lorem);
+
+const lorem1=document.createElement("div")
+lorem1.setAttribute("class","lorem1")
+document.querySelector(".lorem").appendChild(lorem1);
+
+const para8=document.createElement("div")
+para8.setAttribute("class","para8")
+document.querySelector(".lorem").appendChild(para8);
+
+const para7=document.createElement("p")
+para7.innerHTML="Lorem Ipsum is simply dummy text of the printing"
+document.querySelector(".para8").appendChild(para7);
+
+const para9=document.createElement("p")
+para9.innerHTML="Lorem Ipsum is simply dummy text of the printing"
+document.querySelector(".para8").appendChild(para9);
+
+const para10=document.createElement("p")
+para10.innerHTML="Lorem Ipsum is simply dummy text of the printing"
+document.querySelector(".para8").appendChild(para10);
+
+const br21=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br21);
+
+const p21=document.createElement("p")
+p21.innerHTML="2017-2019"
+document.querySelector(".mainclass2").appendChild(p21);
+
+const p22=document.createElement("p")
+p22.innerHTML="Enter Your Job Position Here"
+document.querySelector(".mainclass2").appendChild(p22);
+
+const p23=document.createElement("p")
+p23.innerHTML="Company Name"
+document.querySelector(".mainclass2").appendChild(p23);
+
+const br22=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br22);
+
+
+const dummy =document.createElement("div")
+dummy.setAttribute("class","dummy")
+document.querySelector(".mainclass2").appendChild(dummy);
+
+const lorem2=document.createElement("div")
+lorem2.setAttribute("class","lorem2")
+document.querySelector(".dummy").appendChild(lorem2);
+
+const para11=document.createElement("div")
+para11.setAttribute("class","para11")
+document.querySelector(".dummy").appendChild(para11);
+
+const para12=document.createElement("p")
+para12.innerHTML="Lorem Ipsum is simply dummy text of the printing"
+document.querySelector(".para11").appendChild(para12);
+
+const para13=document.createElement("p")
+para13.innerHTML="Lorem Ipsum is simply dummy text of the printing"
+document.querySelector(".para11").appendChild(para13);
+
+const para14=document.createElement("p")
+para14.innerHTML="Lorem Ipsum is simply dummy text of the printing"
+document.querySelector(".para11").appendChild(para14);
+
+const br23=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br23);
+
+const skill=document.createElement("h5")
+skill.innerHTML="SKILLS_"
+document.querySelector(".mainclass2").appendChild(skill);
+
+const skills=document.createElement("div")
+skills.setAttribute("class","skills")
+document.querySelector(".mainclass2").appendChild(skills);
+
+const skill1=document.createElement("div")
+skill1.setAttribute("class","skill1")
+document.querySelector(".skills").appendChild(skill1);
+
+const photo=document.createElement("p")
+photo.innerHTML="photoshop ✮✮✮✮✰"
+document.querySelector(".skill1").appendChild(photo);
+
+const illu=document.createElement("p")
+illu.innerHTML="illustrate ✮✮✮✮✮"
+document.querySelector(".skill1").appendChild(illu);
+
+const design=document.createElement("p")
+design.innerHTML="InDesign ✮✮✮✰✰"
+document.querySelector(".skill1").appendChild(design);
+
+const skill2=document.createElement("div")
+skill2.setAttribute("class","skill2")
+document.querySelector(".skills").appendChild(skill2);
+
+const xd=document.createElement("p")
+xd.innerHTML="AdobeXD ✮✮✮✮✰"
+document.querySelector(".skill2").appendChild(xd);
+
+const power=document.createElement("p")
+power.innerHTML="Power Point ✮✮✮✮✮"
+document.querySelector(".skill2").appendChild(power);
+
+const effect=document.createElement("p")
+effect.innerHTML="After Effect ✮✮✮✰✰"
+document.querySelector(".skill2").appendChild(effect);
+
+const br24=document.createElement("br")
+document.querySelector(".mainclass2").appendChild(br24);
+
+const interest=document.createElement("h5")
+interest.innerHTML="INTEREST_"
+document.querySelector(".mainclass2").appendChild(interest);
+
+const int=document.createElement("div")
+int.setAttribute("class","int")
+document.querySelector(".mainclass2").appendChild(int);
+
+const travel=document.createElement("p")
+travel.innerHTML="Travelling"
+document.querySelector(".int").appendChild(travel);
+
+const sing=document.createElement("p")
+sing.innerHTML="Singing"
+document.querySelector(".int").appendChild(sing);
+
+const swim=document.createElement("p")
+swim.innerHTML="Swimming"
+document.querySelector(".int").appendChild(swim);
+
+const sketch=document.createElement("p")
+sketch.innerHTML="sketching"
+document.querySelector(".int").appendChild(sketch);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
